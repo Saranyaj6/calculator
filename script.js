285dd7d4f637e4d8f60dfb5b9d17154a2c4ee4ca
saranya06844 <saranyaj72160@gmail.com>
2023-04-03 13:21:16 +0530

    fix the percentage error

--- script.js
+++ script.js
@@ -1,125 +1,95 @@
-const keys = document.querySelectorAll('.key');
-const display_input = document.querySelector('.display .input');
-const display_output = document.querySelector('.display .output');
-
-let input = "";
-
-for (let key of keys) {
-	const value = key.dataset.key;
-
-	key.addEventListener('click', () => {
-		if (value == "clear") {
-			input = "";
-			display_input.innerHTML = "";
-			display_output.innerHTML = "";
-		} else if (value == "backspace") {
-			input = input.slice(0, -1);
-			display_input.innerHTML = CleanInput(input);
-		} else if (value == "=") {
-			let result = eval(PerpareInput(input));
-
-			display_output.innerHTML = CleanOutput(result);
-		} else if (value == "brackets") {
-			if (
-				input.indexOf("(") == -1 || 
-				input.indexOf("(") != -1 && 
-				input.indexOf(")") != -1 && 
-				input.lastIndexOf("(") < input.lastIndexOf(")")
-			) {
-				input += "(";
-			} else if (
-				input.indexOf("(") != -1 && 
-				input.indexOf(")") == -1 || 
-				input.indexOf("(") != -1 &&
-				input.indexOf(")") != -1 &&
-				input.lastIndexOf("(") > input.lastIndexOf(")")
-			) {
-				input += ")";
-			}
-
-			display_input.innerHTML = CleanInput(input);
-		} else {
-			if (ValidateInput(value)) {
-				input += value;
-				display_input.innerHTML = CleanInput(input);
-			}
-		}
-	})
-}
-
-function CleanInput(input) {
-	let input_array = input.split("");
-	let input_array_length = input_array.length;
-
-	for (let i = 0; i < input_array_length; i++) {
-		if (input_array[i] == "*") {
-			input_array[i] = ` <span class="operator">x</span> `;
-		} else if (input_array[i] == "/") {
-			input_array[i] = ` <span class="operator">÷</span> `;
-		} else if (input_array[i] == "+") {
-			input_array[i] = ` <span class="operator">+</span> `;
-		} else if (input_array[i] == "-") {
-			input_array[i] = ` <span class="operator">-</span> `;
-		} else if (input_array[i] == "(") {
-			input_array[i] = `<span class="brackets">(</span>`;
-		} else if (input_array[i] == ")") {
-			input_array[i] = `<span class="brackets">)</span>`;
-		} else if (input_array[i] == "%") {
-			input_array[i] = `<span class="percent">%</span>`;
-		}
-	}
-
-	return input_array.join("");
-}
-
-function CleanOutput (output) {
-	let output_string = output.toString();
-	let decimal = output_string.split(".")[1];
-	output_string = output_string.split(".")[0];
-
-	let output_array = output_string.split("");
-	
-	if (output_array.length > 3) {
-		for (let i = output_array.length - 3; i > 0; i -= 3) {
-			output_array.splice(i, 0, ",");
-		}
-	}
-
-	if (decimal) {
-		output_array.push(".");
-		output_array.push(decimal);
-	}
-
-	return output_array.join("");
-}
-
-function ValidateInput (value) {
-	let last_input = input.slice(-1);
-	let operators = ["+", "-", "*", "/"];
-
-	if (value == "." && last_input == ".") {
-		return false;
-	}
-
-	if (operators.includes(value)) {
-		if (operators.includes(last_input)) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	return true;
-}
-
-function PerpareInput (input) {
-	let input_array = input.split("");
-
-	for (let i = 0; i < input_array.length; i++) {
-		if (input_array[i] == "%") {
-			input_array[i] = "/100";
-		}
-	}
-
-	return input_array.join("");
-}
+// percentage
+function calculatePercentage(displayvalue) {
+    const myString = displayvalue.replace(/[()]/g, ''); 
+        const possibleMatches = ["+", "-", "*", "/"];
+        let lastIndex = -1;
+        for (let i = 0; i < possibleMatches.length; i++) {
+          const index = myString.lastIndexOf(possibleMatches[i]);
+          if (index > lastIndex) {
+            lastIndex = index;
+          }
+        }
+        var start = myString.slice(0, lastIndex);
+        var middle = myString.slice(lastIndex, lastIndex + 1);
+        var last = myString.slice(lastIndex+1,);
+  
+      const result = eval(start);
+      var sec=parseFloat(last);
+  
+      switch (middle) {
+        case "+":
+          var res = result +(result * (sec/100));
+          break;
+        case "-":
+          res = result -(result * (sec/100));
+          break;
+        case "*":
+          res = (result * (sec/100));
+          break;
+        case "/":
+          res = (result / (sec/100));
+        break;
+        default:
+          var expressions=parseFloat(displayvalue);
+          console.log(res)
+          break;
+      }
+      document.querySelector(".calculator .display input").value = res.toFixed(2);
+    }
+    //operators
+    function operators(displayvalue, operator){
+      
+      if(displayvalue.slice(-1) == '+' || displayvalue.slice(-1) == '-' || displayvalue.slice(-1) == '*' || displayvalue.slice(-1) == '/') 
+      { 
+        document.querySelector(".calculator .display input").value = displayvalue.slice(0, -1) + operator;
+      } 
+      else 
+      { 
+        document.querySelector(".calculator .display input").value = displayvalue += operator;
+      };
+    }
+  
+    function frontOperator(displayValue, operator){
+        if(displayValue ==''){
+          if(operator == '*' || operator == "/"){
+            document.querySelector(".calculator .display input").value = '';
+          }
+          else{
+          operators(displayValue, operator); 
+          }
+        }
+        else{
+          operators(displayValue, operator); 
+        }
+  }
+  
+  function togglesign(displayvalue) {
+    const myString =displayvalue;
+        const possibleMatches = ["+", "-", "*", "/"];
+        let lastIndex = -1;
+        for (let i = 0; i < possibleMatches.length; i++) {
+          const index = myString.lastIndexOf(possibleMatches[i]);
+          if (index > lastIndex) {
+            lastIndex = index;
+          }
+        }
+        var start = myString.slice(0, lastIndex);
+        if (myString.includes("(")) {
+          lastIndex = myString.indexOf("(");
+          start = myString.slice(0, lastIndex);
+          var last_val = myString.slice(lastIndex+1,-1)
+          var sign = last_val * -1
+          document.querySelector(".calculator .display input").value = start+sign;
+        }
+        else if(!(lastIndex == -1)){
+          var middle = myString.slice(lastIndex, lastIndex + 1);
+          var last_val = myString.slice(lastIndex+1,);
+          var sign = -1*last_val;
+          document.querySelector(".calculator .display input").value = start+middle+"("+sign+")";
+        }
+        else{
+          sign = myString * -1;
+          document.querySelector(".calculator .display input").value = "("+sign+")";
+        }
+  }
+  
